f78fcb0396dd07d7c28527b855c76076c92d2564
Mariana <mlobosilva@outlook.com>
2022-10-31 18:07:03 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,32 +1,81 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { userEmail } from '../redux/actions';
 
 class Login extends React.Component {
-  // state = {
-  //   disabled: true,
-  // };
+  state = {
+    charsEmail: '',
+    charsPassword: '',
+    passwordSixChars: false,
+  };
+
+  getPasswordChars = ({ target }) => {
+    this.setState({ charsPassword: target.value }, this.disabledButton);
+  };
+
+  getEmailChars = ({ target }) => {
+    this.setState({ charsEmail: target.value }, this.disabledButton);
+  };
 
   disabledButton = () => {
+    const { charsPassword, charsEmail } = this.state;
+    const six = 6;
+    const emailValidation = /\S+@\S+\.\S+/;
+    if (charsPassword.length >= six && emailValidation.test(charsEmail)) {
+      this.setState({ passwordSixChars: true });
+    } else {
+      this.setState({ passwordSixChars: false });
+    }
+  };
 
+  loginButton = () => {
+    const { charsEmail } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(userEmail(charsEmail));
+
+    history.push('/carteira');
   };
 
   render() {
+    const { passwordSixChars } = this.state;
     return (
       <form>
-        <label htmlFor="inputEmail" data-testid="email-input">
-          <input name="inputEmail" id="inputEmail" type="email" />
-        </label>
-        <label htmlFor="inputPassword" data-testid="password-input">
-          <input
-            name="inputPassword"
-            id="inputPassword"
-            type="password"
-            minLength={ 6 }
-          />
-        </label>
-        <button type="button" disabled={ this.disabledButton }>Entrar</button>
+        <input
+          data-testid="email-input"
+          name="inputEmail"
+          id="inputEmail"
+          type="email"
+          onChange={ this.getEmailChars }
+        />
+        <input
+          data-testid="password-input"
+          name="inputPassword"
+          id="inputPassword"
+          type="password"
+          minLength={ 6 }
+          onChange={ this.getPasswordChars }
+        />
+        <button
+          type="button"
+          disabled={ !passwordSixChars }
+          onClick={ this.loginButton }
+        >
+          Entrar
+
+        </button>
       </form>
     );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  userReducer: state.user,
+});
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape().isRequired,
+};
+
+export default connect(mapStateToProps)(Login);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,5 +1,10 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
