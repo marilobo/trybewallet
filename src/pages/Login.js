b305083760535978fdde50185debd95443ead6c8
Mariana <mlobosilva@outlook.com>
2022-10-31 16:04:16 -0300

    Configurando o redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 class Login extends React.Component {
+  // state = {
+  //   disabled: true,
+  // };
+
+  disabledButton = () => {
+
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <label htmlFor="inputEmail" data-testid="email-input">
+          <input name="inputEmail" id="inputEmail" type="email" />
+        </label>
+        <label htmlFor="inputPassword" data-testid="password-input">
+          <input
+            name="inputPassword"
+            id="inputPassword"
+            type="password"
+            minLength={ 6 }
+          />
+        </label>
+        <button type="button" disabled={ this.disabledButton }>Entrar</button>
+      </form>
+    );
   }
 }
 
