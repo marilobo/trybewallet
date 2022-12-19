04698bdf38480b414870ece61e97a67769c49859
Mariana <mlobosilva@outlook.com>
2022-12-19 12:12:24 -0300

    Faz alterações na página carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,12 +2,15 @@ import React from 'react';
 // import Header from '../components/Header';
 import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
+import '../css/container.css';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
-        <WalletForm />
+        <div className="central-container">
+          <WalletForm />
+        </div>
         <Table />
       </div>
     );
